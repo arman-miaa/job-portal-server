2f7ce0fa2afa6040fb707ec2e2ea1588f27c9088
Arman <mdarman365506@gmail.com>
2024-12-13 15:46:21 +0600

    added patch api for update status

--- index.js
+++ index.js
@@ -87,12 +87,63 @@ async function run() {
       res.send(result);
     });
 
+
+
+    app.get("/job-applications/jobs/:job_id", async (req, res) => {
+      const jobId = req.params.job_id;
+      const query = { job_id: jobId };
+      const result = await jobApplycationCollection.find(query).toArray();
+      res.send(result);
+});
+
+
     app.post('/job-applications', async (req, res) => {
       const application = req.body;
       const result = await jobApplycationCollection.insertOne(application);
+
+      // Not the best way (use aggregate)
+      // skip --> it
+
+      const id = application.job_id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobsCollection.findOne(query);
+      console.log(job);
+      let newCount = 0;
+      if (job.applicationCount) {
+        newCount = job.applicationCount + 1;
+      }
+      else {
+        newCount = 1;
+      }
+
+      // now update the job info
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          applicationCount: newCount
+        }
+      }
+      const updateResult = await jobsCollection.updateOne(filter,updatedDoc)
+
+
+
       res.send(result);
     })
     
+
+    app.patch("/job-applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: data.status
+        }
+      }
+      const result = await jobApplycationCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+});
+
 
 
   } finally {
