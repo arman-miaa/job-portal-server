b0c8cf163c2d28b7b01d39f2806756f0de19dcb9
Arman <mdarman365506@gmail.com>
2024-12-13 11:13:08 +0600

    added job api for get dynamic job

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-
+// const uri = "mongodb://localhost:27017/";
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7argw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -36,8 +36,13 @@ async function run() {
     const jobApplycationCollection = client.db("jobportal").collection('job_application');
 
     // job releted APIs
-      app.get('/jobs', async (req, res) => {
-          const cursor = jobsCollection.find();
+    app.get('/jobs', async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = {hr_email: email}
+      }
+          const cursor = jobsCollection.find(query);
           const result = await cursor.toArray();
           res.send(result);
       })
@@ -50,6 +55,7 @@ async function run() {
       })
     
     app.post('/jobs', async (req, res) => {
+
       const newJob = req.body;
       const result = await jobsCollection.insertOne(newJob);
       res.send(result);
